d3dc223dc941a2ddb83c69fcbbf8c768dde562df
Mitchell Kampert <mitch.kampert@gmail.com>
2022-08-31 23:48:27 +0200

    added base ai-auto drive

--- python_driver-ai/manual.py
+++ python_driver-ai/manual.py
@@ -2,14 +2,13 @@ import pygame
 import os
 import math
 import sys
-import neat
 
 # Map image dimensions
-SCREEN_WIDTH = 1485
+SCREEN_WIDTH = 1244
 SCREEN_HEIGHT = 1016
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-TRACK = pygame.image.load(os.path.join("img", "track3.png"))
+TRACK = pygame.image.load(os.path.join("img", "track.png"))
 
 class Car(pygame.sprite.Sprite):
     def __init__(self):
@@ -20,28 +19,27 @@ class Car(pygame.sprite.Sprite):
         self.image = self.original_image
         
         self.rect = self.image.get_rect(center=(490, 820))
+        self.drive_state = False
         self.vel_vector = pygame.math.Vector2(0.8, 0)
         self.angle = 0
         self.rotation_vel = 5
         self.direction = 0
         self.alive = True
-        self.radars = []
 
     
     # Update car position
     def update(self):
-        self.radars.clear()
         self.drive()
         self.rotate()
         # set specific radar angles
         for radar_angle in (-60, -30, 0, 30, 60):
             self.radar(radar_angle)
         self.collision()
-        self.data()
     
     # Drive forward if drive state = True by vector amount
     def drive(self):
-        self.rect.center += self.vel_vector * 6
+        if self.drive_state:
+            self.rect.center += self.vel_vector * 6
 
     
     # Turn vehicle
@@ -87,40 +85,11 @@ class Car(pygame.sprite.Sprite):
         pygame.draw.line(SCREEN, (255, 255, 255, 255), self.rect.center, (x, y), 1)
         pygame.draw.circle(SCREEN, (0, 255, 0, 0), (x, y), 3)
 
-        dist = int(math.sqrt(math.pow(self.rect.center[0] - x, 2) + math.pow(self.rect.center[1] - y, 2)))
 
-        self.radars.append([radar_angle, dist])
-
-
-    def data(self):
-        input = [0, 0, 0, 0, 0]
-        for i, radar in enumerate(self.radars):
-            input[i] = int(radar[1])
-        return input
+car = pygame.sprite.GroupSingle(Car())
 
 
-
-def remove(index):
-    cars.pop(index)
-    ge.pop(index)
-    nets.pop(index)
-
-
-def eval_genomes(genomes, config):
-    global cars, ge, nets
-
-    cars = []
-    ge = []
-    nets = []
-
-    for genome_id, genome in genomes:
-        cars.append(pygame.sprite.GroupSingle(Car()))
-        ge.append(genome)
-        net = neat.nn.FeedForwardNetwork.create(genome, config)
-        nets.append(net)
-        genome.fitness = 0
-
-
+def eval_genomes():
     run = True
     while run:
         for event in pygame.event.get():
@@ -131,52 +100,27 @@ def eval_genomes(genomes, config):
         # Display track within window
         SCREEN.blit(TRACK, (0, 0))
 
-        if len(cars) == 0:
-            break
-
-        for i, car in enumerate(cars):
-            ge[i].fitness += 1
-            if not car.sprite.alive:
-                remove(i)
-
-        for i, car in enumerate(cars):
-            output = nets[i].activate(car.sprite.data())
-            if output[0] > 0.7:
-                car.sprite.direction = 1
-            if output[1] > 0.7:
-                car.sprite.direction = -1
-            if output[0] <= 0.7 and output[1] <= 0.7:
-                car.sprite.direction = 0
-
+        # User input
+        user_input = pygame.key.get_pressed()
+        
+        # Drive state False when no keys pressed
+        if sum(pygame.key.get_pressed()) <= 1:
+            car.sprite.drive_state = False
+            car.sprite.direction = 0
+        
+        # User controls
+        if user_input[pygame.K_w]:
+            car.sprite.drive_state = True
+        if user_input[pygame.K_a]:
+            car.sprite.direction = -1
+        elif user_input[pygame.K_d]:
+            car.sprite.direction = 1
 
         # Update
-        for car in cars:
-            car.draw(SCREEN)
-            car.update()
+        car.draw(SCREEN)
+        car.update()
         pygame.display.update()
 
 
-# NEAT setup
-def run(config_path):
-    global pop
-    config = neat.config.Config(
-        neat.DefaultGenome,
-        neat.DefaultReproduction,
-        neat.DefaultSpeciesSet,
-        neat.DefaultStagnation,
-        config_path
-    )
 
-    pop = neat.Population(config)
-
-    pop.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    pop.add_reporter(stats)
-
-    pop.run(eval_genomes, 50)
-
-
-if __name__ == '__main__':
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, "config.txt")
-    run(config_path)
+eval_genomes()
